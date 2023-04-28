cb70272145da80c9d50ebae4caab18b2e93c0361
avaskivgithub <andriana.vaskiv@gmail.com>
2023-04-27 21:13:44 -0400

    Draft example with http get / post and mocha+chai

---
+++ javascript/ApiTests/README.md
@@ -0,0 +1,15 @@
+
+## Setup
+```
+npm init
+npm install chai --save-dev
+npm install chai-http --save-dev
+npm install mocha --save-dev
+npm install mocha-junit-reporter --save-dev
+```
+
+## TBD
+* https://www.chaijs.com/plugins/chai-http/ - chai http didn't work for me
+* https://blog.logrocket.com/testing-node-js-mocha-chai/
+* https://mochajs.org/
+* https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
---
+++ javascript/ApiTests/tests/http_requests.js
@@ -0,0 +1,58 @@
+var chai = require('chai')
+  , chaiHttp = require('chai-http')
+  , expect = require('chai').expect;
+
+  chai.use(chaiHttp);
+
+
+  describe('#http requests:', function() {
+    this.timeout(17000);
+
+    beforeEach(function() {
+    })
+
+    it('test simple get request', async function() {
+
+        const response = await fetch("https://reqres.in/api/users?page=2");
+        const jsonData = await response.json();
+        
+        var expElem = {
+            id: 7,
+            email: 'michael.lawson@reqres.in',
+            first_name: 'Michael',
+            last_name: 'Lawson',
+            avatar: 'https://reqres.in/img/faces/7-image.jpg'
+          };
+        expect(response.status).to.be.equal(200);
+        expect(jsonData.data[0]).to.deep.equal(expElem);
+    })
+
+    it('test get request with delay', async function() {
+
+      const response = await fetch("http://uitestingplayground.com/ajaxdata");
+      const data = await response.text();
+
+      expect(response.status).to.be.equal(200);
+      expect(data).to.deep.equal('Data loaded with AJAX get request.');
+  })
+
+    it('test simple post request', async function() {
+
+      var data = {
+        "name": "morpheus",
+        "job": "leader"
+      };
+      const response = await fetch("https://reqres.in/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+      });
+      const jsonData = await response.json();
+
+      expect(response.status).to.be.equal(201);
+      expect(jsonData).to.deep.contains(data);
+  })
+
+})
