e58b71f9ce46ee2055630de38820fdb08e342bbe
Andriana <andriana.vaskiv@gmail.com>
2025-02-15 22:23:32 -0500

    POC exeample updated with selecting some location

--- python/web_scraping/poc_example_ubereats.py
+++ python/web_scraping/poc_example_ubereats.py
@@ -8,8 +8,11 @@ README:
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import csv
 
@@ -49,17 +52,43 @@ if __name__ == '__main__':
 
     start = time.time()
     # URL of the data
-    # url = "https://www.ubereats.com/ca/collection-page?collectionName=ca-foodflavors-chocolate"
-    # url = "https://www.ubereats.com/ca/store/rades-restaurant/PLu8iobhUb6WCGNzZqVfWQ?diningMode=DELIVERY"
-    url = "https://www.ubereats.com/store/aloy-thai-cuisine/WceXtnnCRNSIoixZqneUqg?diningMode=DELIVERY"
-    # url = "https://www.ubereats.com/ca/store/mcdonalds-northfield-%26-davenport/PmeLkXqMTJargw2CByApjQ?diningMode=DELIVERY"
+    url = "https://www.ubereats.com"
 
     options = Options()
     options.headless = True
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install(), options=options)
     driver.get(url)
-    time.sleep(3) # proper implicit timeout needed
 
+    # Close pop up about auto location (if it appears)
+    try:
+        elem_close_button = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'button[title="Close"]'))
+            )
+        elem_close_button.click()
+        time.sleep(2)
+    except:
+        pass
+
+    # Get 2nd location from the drop down list
+    elem = driver.find_element(By.CSS_SELECTOR, 'input[role="combobox"]')
+    elem.send_keys("London Ontario Canada")
+    time.sleep(2)
+    elem.send_keys(Keys.ARROW_DOWN)
+    time.sleep(0.5)
+    elem.send_keys(Keys.ARROW_DOWN)
+    time.sleep(0.5)
+    elem.send_keys(Keys.ENTER)
+    time.sleep(5)
+
+    # Get all available store links and follow the last in the list
+    elems = driver.find_elements(By.CSS_SELECTOR, 'a[data-testid="store-card"]')
+    links = [elem.get_attribute('href') for elem in elems]
+    print(links)
+
+    driver.get(elems[-1].get_attribute('href'))
+    time.sleep(5)
+
+    # Get items names and prices
     try:
         menu_items_all = scroll_and_get_all_data(driver, selector="//div[1]/div/span", selector_type=By.XPATH)
         menu_items = [el for el in menu_items_all if el not in [' • ', '']]
